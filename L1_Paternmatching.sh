490613ffdbd003d4ac8ba11112f31b8289dd6458
geoffrey-owden <geoffrey.owden@ibm.com>
2020-06-14 08:30:06 +0100

    adding pattern matching

--- L1_Paternmatching.sh
+++ L1_Paternmatching.sh
@@ -6,8 +6,15 @@ INPUTSTRING=rababarabarabarara
 echo
 echo INPUTSTRING is $INPUTSTRING
 echo
-echo 'the result of ##*ba is ' ${INPUTSTRING##*ba}
-echo 'the result of #*ba is ' ${INPUTSTRING#*ba} 
-echo 'the result of %%ba* is ' ${INPUTSTRING%%ba*}
-echo 'the result of %ba* is ' ${INPUTSTRING%ba*}
+echo 'From the left to the right, this pattern ##*ba will match the last/longest pattern that matches ba' 
+echo 'the result of ##*ba is     >>>>>    ' ${INPUTSTRING##*ba}
+echo
+echo 'From the left to the right, this pattern #*ba will match the first/shortest pattern that matches ba'
+echo 'the result of #*ba is     >>>>>     ' ${INPUTSTRING#*ba} 
+echo
+echo 'From the right to the left, the pattern %%ba will match the last/longest pattern that matches ba'
+echo 'the result of %%ba* is     >>>>>     ' ${INPUTSTRING%%ba*}
+echo
+echo 'From the right to the left, the pattern %ba will match the first/shortest pattern that matches ba'
+echo 'the result of %ba* is     >>>>>     ' ${INPUTSTRING%ba*}
 echo
